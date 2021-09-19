e1757eaecb40ecc8c154b63dfef1afed0a03617d
palash sarkar <spalash521@gmail.com>
2021-09-19 16:34:03 +0300

    added my last

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExtranalUser></ExtranalUser>
 
 
 
@@ -15,6 +16,39 @@ function App() {
 }
 
 
+
+
+
+
+
+function ExtranalUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(user => setUsers(user))
+  }, [])
+  return (
+    <div>
+      <h1>External user</h1>
+      {
+        users.map(user => <User name={user.name
+
+        } email={user.email} > </User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div className="product">
+      <h2> name: {props.name}</h2>
+      <p> Email: {props.email}</p>
+
+    </div>
+  )
+}
 
 
 function Counter() {
